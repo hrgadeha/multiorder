a8296dda6706717e47f952e416f9dcb88c5baec2
Your Name <you@example.com>
2018-09-13 18:58:23 +0530

    Add validation on date, update event to create multiple SO

---
+++ multiorder/multiorder/doctype/multiorder/multiorder.js
@@ -0,0 +1,112 @@
+// Copyright (c) 2018, Frappe Technologies Pvt. Ltd. and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on("MultiOrder Item", "item_code", function(frm, cdt, cdn) {
+	$.each(frm.doc.multiorder_item || [], function(i, d) {
+		d.delivery_date = frm.doc.delivery_date;
+	});
+	refresh_field("multiorder_item");
+});
+
+frappe.ui.form.on("MultiOrder Item", "qty", function(frm, cdt, cdn){
+
+	cur_frm.refresh();
+	cur_frm.refresh_fields();
+	var d = locals[cdt][cdn];
+	frappe.model.set_value(d.doctype, d.name, "amount", (d.qty * d.rate));
+
+	var multiorder_item = frm.doc.multiorder_item;
+  	var net_total = 0;
+	for (var i in multiorder_item){
+		net_total = net_total + multiorder_item[i].amount
+		cur_frm.set_value("net_total",net_total.toFixed(2));
+		cur_frm.set_value("grand_total",net_total.toFixed(2));
+		}
+});
+
+frappe.ui.form.on("MultiOrder Item", "rate", function(frm, cdt, cdn){
+
+	cur_frm.refresh();
+	cur_frm.refresh_fields();
+	var d = locals[cdt][cdn];
+	frappe.model.set_value(d.doctype, d.name, "amount", (d.qty * d.rate));
+
+	var multiorder_item = frm.doc.multiorder_item;
+  	var net_total = 0;
+	for (var i in multiorder_item){
+		net_total = net_total + multiorder_item[i].amount
+		cur_frm.set_value("net_total",net_total.toFixed(2));
+		cur_frm.set_value("grand_total",net_total.toFixed(2));
+		}
+});
+
+frappe.ui.form.on("MultiOrder Item", "multiorder_item_remove", function(frm, cdt, cdn){
+
+	cur_frm.refresh();
+	cur_frm.refresh_fields();
+	var d = locals[cdt][cdn];
+
+	var multiorder_item = frm.doc.multiorder_item;
+  	var net_total = 0;
+	for (var i in multiorder_item){
+		net_total = net_total + multiorder_item[i].amount
+		cur_frm.set_value("net_total",net_total.toFixed(2));
+		cur_frm.set_value("grand_total",net_total.toFixed(2));
+		}
+});
+
+/*frappe.ui.form.on("Task", "validate", function(frm) {
+    if (frm.doc.from_date < get_today()) {
+        frappe.msgprint(__("You can not select past date in From Date"));
+        frappe.validated = false;
+    }
+});*/
+
+frappe.ui.form.on("MultiOrder", {
+   validate: function(frm) {
+	if (frm.doc.delivery_date < frm.doc.date){
+		frappe.msgprint("Please Select Delivery Date Greater Than Or Equal To Posting Date");
+		frappe.validated = false;
+	}
+    },
+
+    delivery_date: function(frm) {
+	if (frm.doc.delivery_date < frm.doc.date){
+		frappe.msgprint("Please Select Delivery Date Greater Than Or Equal To Posting Date");
+	}
+    },
+
+   additional_discount_percentage: function(frm) {
+	if (frm.doc.apply_discount_on == "Net Total"){
+		cur_frm.set_value("grand_total",frm.doc.net_total);
+		var additional_discount_amount = (frm.doc.additional_discount_percentage / 100) * frm.doc.net_total;
+		var grand_total = frm.doc.net_total - additional_discount_amount;
+		cur_frm.set_value("additional_discount_amount",additional_discount_amount.toFixed(2));
+		cur_frm.set_value("grand_total",grand_total.toFixed(2));
+		}
+	else if (frm.doc.apply_discount_on == "Grand Total"){
+		cur_frm.set_value("grand_total",frm.doc.net_total);
+		var additional_discount_amount = (frm.doc.additional_discount_percentage / 100) * frm.doc.grand_total;
+		var grand_total = frm.doc.net_total - additional_discount_amount;
+		cur_frm.set_value("additional_discount_amount",additional_discount_amount.toFixed(2));
+		cur_frm.set_value("grand_total",grand_total.toFixed(2));	
+		}
+    },
+
+   apply_discount_on: function(frm) {
+	if (frm.doc.apply_discount_on == "Net Total"){
+		cur_frm.set_value("grand_total",frm.doc.net_total);
+		var additional_discount_amount = (frm.doc.additional_discount_percentage / 100) * frm.doc.net_total;
+		var grand_total = frm.doc.net_total - additional_discount_amount;
+		cur_frm.set_value("additional_discount_amount",additional_discount_amount.toFixed(2));
+		cur_frm.set_value("grand_total",grand_total.toFixed(2));
+		}
+	if (frm.doc.apply_discount_on == "Grand Total"){
+		cur_frm.set_value("grand_total",frm.doc.net_total);
+		var additional_discount_amount = (frm.doc.additional_discount_percentage / 100) * frm.doc.net_total;
+		var grand_total = frm.doc.net_total - additional_discount_amount;
+		cur_frm.set_value("additional_discount_amount",additional_discount_amount.toFixed(2));
+		cur_frm.set_value("grand_total",grand_total.toFixed(2));
+		}
+    }
+});
---
+++ multiorder/multiorder/doctype/multiorder/multiorder.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2018, Frappe Technologies Pvt. Ltd. and contributors
+# For license information, please see license.txt
+
+from __future__ import unicode_literals
+import frappe
+from frappe import msgprint
+from frappe.model.document import Document
+from frappe.utils import money_in_words
+
+class MultiOrder(Document):
+	def on_submit(doc):
+		frappe.msgprint(len(doc.multiorder_item))
+		doc.in_words = money_in_words(doc.grand_total)
+		for c in doc.multiorder_customer:
+			items = []
+			for d in doc.multiorder_item:
+				item_li = {"item_code": d.item_code,"qty": d.qty,"rate": d.rate,"warehouse":d.warehouse}
+				items.append(item_li)
+			sales_order = frappe.get_doc({
+			"doctype": "Sales Order", 
+			"customer": c.customer, 
+			"delivery_date": "2018-09-23",
+			"apply_discount_on":doc.apply_discount_on,
+			"additional_discount_percentage":doc.additional_discount_percentage,
+			"discount_amount":doc.additional_discount_amount,
+			"net_total":doc.net_total,
+			"grand_total":doc.grand_total,
+			"in_words":doc.in_words,
+			"tc_name":doc.terms,
+			"items": items
+			})
+			sales_order.insert(ignore_permissions=True)
+			sales_order.submit()
